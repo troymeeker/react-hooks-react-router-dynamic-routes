82c898db23d667c966c188ccd717c29ccdd63ed6
troymeeker <troymeeker1@gmail.com>
2021-11-02 11:38:52 -0700

    complete

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
+
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+  // console.log(match);
+
   return (
     <div>
-      <MoviesList movies={movies} />
+      <MoviesList movies={movies}/>
+      <Route exact path={match.url}>
+        <h3>Choose a movie to go to its specific page</h3>
+      </Route> 
+      <Route path={`${match.url}/:movieId`}>
+      <MovieShow movies={movies}/>
+      </Route>
     </div>
-  );
+    );
 }
+
 export default MoviesPage;
